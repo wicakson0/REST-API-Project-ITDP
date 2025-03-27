a4b40f207c212f16d7f4bae37124c511da5a5d64
donitanhero <doni.tan55@gmail.com>
2025-03-27 13:37:23 +0700

    Remove Task3 (combined with task 2), move postman docs into independent folder

--- Task3/src/config/Api.jsx
+++
@@ -1,6 +0,0 @@
-const Domain = "http://localhost:5000";
-const Api = `${Domain}/api/contacts`;
-const ApiEdit = `${Domain}/api/contacts/:id`;
-const Url = `${Domain}/images`;
-
-export { Domain, Api, ApiEdit, Url };
--- Task3/src/pages/Services.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ContactList from "../components/crud/ContactList";
-
-const Services = () => {
-  return <ContactList />;
-};
-
-export default Services;
